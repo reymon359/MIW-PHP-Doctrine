e471d875748db8c28f047edc1348f269f8e3f642
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-11-26 07:58:53 +0100

    working on new user form

--- src/controllers.php
+++ src/controllers.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * PHP version 7.3
+ * src/controllers.php
+ *
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es/ ETS de Ingeniería de Sistemas Informáticos
+ */
+
 use MiW\Results\Entity\User;
 use MiW\Results\Utils;
 
@@ -7,21 +15,63 @@ function funcionHomePage()
 {
     global $routes;
 
-    $rutaListado = $routes->get('ruta_user_list')->getPath();
-    echo <<< MARCA_FIN
+    $rutaListadoUsers = $routes->get('ruta_user_list')->getPath();
+    echo <<< ____MARCA_FIN
     <ul>
-        <li><a href="$rutaListado">Listado Usuarios</a></li>
+        <li><a href="$rutaListadoUsers">Listado Users</a></li>
     </ul>
-MARCA_FIN;
+____MARCA_FIN;
 }
-
 
 function funcionListadoUsers(): void
 {
+    global $routes;
 
     $entityManager = Utils::getEntityManager();
-
-    $userRepository = $entityManager->getRepository(User::class);
-    $users = $userRepository->FindAll();
+    $usersRepository = $entityManager->getRepository(User::class);
+    $users = $usersRepository->findAll();
     var_dump($users);
+
+    // Enlace Nuevo User
+    $rutaNuevoUser = $routes->get('ruta_user_nuevo')->getPath();
+    echo "<a href='$rutaNuevoUser'>Nuevo User</a>";
+}
+
+function funcionNuevoUser()
+{
+    global $routes, $context;
+    var_dump($context);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') { // método GET => muestro formulario
+        $rutaNuevoUser = $routes->get('ruta_user_nuevo')->getPath();
+        echo <<< ___MARCA_FIN
+    <form method="POST" action="$rutaNuevoUser">
+        Nombre: <input type="text" name="nombre" required>
+        Email: <input type="email" name="email" required>
+        Password: <input type="password" name="password" required>
+        Enabled: <input type="checkbox" name="enabled" value="true"> 
+        isAdmin: <input type="checkbox" name="isAdmin" value="true"> 
+        <input type="submit" value="Enviar"> 
+    </form>
+___MARCA_FIN;
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {    // método POST => proceso formulario
+        $entityManager = Utils::getEntityManager();
+
+        $nombre = $_POST['nombre'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $enabled = $_POST['enabled'] ?? true;
+        $isAdmin = $_POST['isAdmin'] ?? true;
+
+        $user = new User($nombre, $graduacion);
+
+        // Hacer persistente los datos
+        $entityManager->persist($bebida);
+        $entityManager->flush();
+        var_dump($bebida);
+
+        // Enlace Listado Users
+        $rutaListadoUsers = $routes->get('ruta_user_list')->getPath();
+        echo "<a href='  $rutaListadoUsers'>Listado Users</a>";
+    }
 }
